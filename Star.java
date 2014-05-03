78baef16444df807e5b142b1bfb13dd3686b0acd
mintguy <arnautov.dmitriy@yandex.ru>
2014-05-03 20:30:54 +0400

    second commit

---
+++ Star.java
@@ -0,0 +1,21 @@
+
+public class Star {
+
+	
+	public static void main(String[] args) {
+		String first_m[][] = new String[12][2];
+		first_m[0][0] = "David";
+		first_m[0][1] = "Darrel";
+		char num;
+		num = 88;
+		if (num == 88){
+		System.out.println("Это простая программа выводит букву " + num); 
+		}
+		else {
+		System.out.println("Да, все отлично работает"); 
+		}
+		int elementOfMassive = first_m.length; 
+		System.out.println("Да, просто работает и выводит массив " + "\n" + elementOfMassive); 
+
+}
+}
